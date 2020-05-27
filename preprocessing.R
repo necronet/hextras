c9a028a1bff2a126f871b0b01598ae25c76024e3
Jose Ayerdis <joseayerdis@gmail.com>
2020-05-26 22:00:24 -0600

    some minor tweaks

--- preprocessing.R
+++ preprocessing.R
@@ -5,7 +5,7 @@ library(stringr)
 library(lubridate)
 library(r2excel)
 
-ignore_workers = c(17, 88, 116, 190)
+ignore_workers = c(17, 116, 190)
 
 workers <-  read_excel("data/trabajadores_horas_extras.xlsx")
 
@@ -21,7 +21,7 @@ worker_workday <- workers %>% filter(!(ID %in% ignore_workers)) %>% mutate( Regi
             filter(str_detect(Fecha, "\\d{2}\\/\\d{2}\\/\\d{4}")) %>% filter(Registros != '--') %>%
             filter(!is.na(Registros)) %>% select(ID, Fecha, Registros, Obs)  %>% 
             separate(Registros, c('T1','T2','T3','T4'), sep = "\\|") %>% mutate_all(str_trim) %>%
-            filter(!is.na(T4) & ID %in% c(7,11) )
+            filter(!is.na(T4) & ID %in% c(88) )
             
 
 lubridate::dmy_hms('11/05/2020 07:01:32')
@@ -37,12 +37,14 @@ xlsx.addTitle <- function(sheet, rows = NULL, rowIndex, colIndex, title, titleSt
   return (rows)
 }
 
+WORKDAY_HOUR <- 9.6
+
 dumpTable <- function(currentID = 7) {
   worker_workday %>% filter(ID == currentID) %>% select(-Obs) %>% mutate_at(vars(contains("T")), ~lubridate::dmy_hms(paste0(Fecha, .))) %>% 
     mutate_at(vars(contains("T")), ~floor_date(., unit = "minute")) %>%
     mutate(lunch = T3 - T2, lunch_delta = pmax(0, (as.numeric(lunch) - 30)/60 )) %>%
     mutate( total_workday = T4 - T1 ) %>%
-    mutate(workday_total_hours = as.numeric(total_workday) - 9.6 ) %>% 
+    mutate(workday_total_hours = as.numeric(total_workday) - WORKDAY_HOUR ) %>% 
     mutate(total_workday_with_lunch = workday_total_hours -lunch_delta ) %>%
     mutate(ENTRADA = format(T1, "%H:%M %p"), `SALIDA A ALMUERZO` = format(T2, "%H:%M %p"),
            `ENTRADA ALMUERZO` = format(T3, "%H:%M %p"), `HORA ALMUERZO`=paste0(as.numeric(lunch), " min"),
@@ -50,11 +52,11 @@ dumpTable <- function(currentID = 7) {
            `0.5 HORA ALMUERZO SE COMPUTA TIEMPO EFECTIVO`="12:30 AM",
            `SALIDA` = format(T4, "%H:%M %p"),
            `TIEMPO CONVERTIDO A HORAS`=sprintf("%.2f", total_workday),
-           `TIEMPO EXTRAORDINARIO = TIEMPO EFECTIVO - 9.60 HORAS LUNES A VIERNES` = sprintf("%.2f",workday_total_hours),
+           `TIEMPO EXTRAORDINARIO = TIEMPO EFECTIVO - 9 HORAS LUNES A VIERNES` = sprintf("%.2f",workday_total_hours),
            `TIEMPO ALMUERZO MAYOR DE 30 MINUTOS` = sprintf("%.2f",total_workday_with_lunch)) %>%
     select(Fecha, ENTRADA, `SALIDA A ALMUERZO`, `ENTRADA ALMUERZO`, `HORA ALMUERZO`, `SOBRE EXCESO T/ ALMUERZO`,
            `0.5 HORA ALMUERZO SE COMPUTA TIEMPO EFECTIVO`, `SALIDA`, `TIEMPO CONVERTIDO A HORAS`,
-           `TIEMPO EXTRAORDINARIO = TIEMPO EFECTIVO - 9.60 HORAS LUNES A VIERNES`,
+           `TIEMPO EXTRAORDINARIO = TIEMPO EFECTIVO - 9 HORAS LUNES A VIERNES`,
            `TIEMPO ALMUERZO MAYOR DE 30 MINUTOS`) %>%
     pivot_longer(-Fecha, names_to = "PERIODO 2º MAYO 2020", values_to="Hora") %>%
     pivot_wider(names_from = "Fecha", values_from = "Hora")
